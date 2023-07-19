8d8b298527f56c26a07930a202a72491b9a086db
chapmanpiper163 <139989702+chapmanpiper163@users.noreply.github.com>
2023-07-19 10:38:08 -0400

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_gq7an17", "template_xe7frwd", formData)
+    emailjs.send("service_9fbt6bp", "template_vz6xjav", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
